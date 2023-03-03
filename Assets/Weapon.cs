346205e88905de174cdb5ea8cfb5bc1c86fa0385
Thai Quan <105270206+SWP391-SE1615-NET-Nhom3@users.noreply.github.com>
2023-03-03 22:40:52 +0700

    update

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePoint;
     public float fireForce = 20f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +16,14 @@ public class Weapon : MonoBehaviour
     }
     public void Fire()
     {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse); 
+
+        GameObject bullet1 = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        bullet1.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
+
+        // Tạo đối tượng bullet2
+        GameObject bullet2 = Instantiate(bulletPrefab, firePoint.position + new Vector3(0, 1, 0), Quaternion.identity);
+        bullet2.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
+
     }
     // Update is called once per frame
     void Update()
