18d5908595a2b0b042575fd104ee6e8db848a4ca
huytq <105337431+HuyTQ-FPT@users.noreply.github.com>
2023-03-03 22:19:41 +0700

    sound

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -8,6 +8,8 @@ public class Weapon : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePoint;
     public float fireForce = 20f;
+    public AudioSource gun;
+    public AudioClip bullet1;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,8 @@ public class Weapon : MonoBehaviour
     public void Fire()
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse); 
+        bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
+        gun.PlayOneShot(bullet1);
     }
     // Update is called once per frame
     void Update()
